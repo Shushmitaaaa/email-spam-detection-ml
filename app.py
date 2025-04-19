81574e1a586e0b5ce8251bd3ea6be1b04b7aacc2
Shushmita Tiwari <Shushmitat39@gmail.com>
2025-04-19 07:31:26 +0530

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.data.path.append('./nltk_data')
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 ps=PorterStemmer()
 
